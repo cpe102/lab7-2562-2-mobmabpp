dcc8dd6016d74b0c29c30ea323c2d737d6164e4e
lab314-1 <lab314-1@labeng.local>
2020-01-21 17:06:32 +0700

    LAB7

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -3,7 +3,24 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+string findGrade(int N){
+	if(N>90){
+		return "A";
+	}
+	if(N<=90&&N>75){
+		return "B";
+	}
+	if(N<=75&&N>60){
+		return "C";
+	}
+	if(N<=60&&N>45){
+		return "D";
+	}
+	if(N<=45){
+		return "F";
+	}
+
+}
 
 int main(){
 	//Input the number of students
@@ -17,9 +34,9 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin>>score[i];
 		i++;
 	}
 	
